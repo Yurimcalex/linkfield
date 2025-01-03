688d31a6e73c2896db1d030444833a1872fba5f3
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-03 22:48:41 +0200

    Create read text from a file feat

--- src/main.js
+++ src/main.js
@@ -1,2 +1,17 @@
-import './style.css'
+import './style.css';
 
+
+async function readTextFromFile(file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  return text;
+}
+
+
+async function render() {
+  const textData = await readTextFromFile('initialLinkList.txt');
+  console.log(textData);
+}
+
+
+render();
