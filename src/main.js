d7d8e26ab779307bdbfaf431f0be30fd4765a9b9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-04 14:09:55 +0200

    Refact fn to parse links data as text

--- src/main.js
+++ src/main.js
@@ -10,23 +10,32 @@ async function readTextFromFile(file) {
 
 function parseText(text) {
   const blockDelimiter = '---------------------------------------------------------';
+  const linkDelimiter = '\n\n';
+  const linkItemDelimiter = '\n';
+  
   return text
     .split(blockDelimiter)
-    .filter(block => !!block)
-    .map(block => block.trim())
-    .map(block => {
-      const blockData = block.split('\n\n');
-      return {
-        title: blockData[0].slice(2),
-        links: blockData.slice(1).map(txt => {
-          const linkData = txt.split('\n');
-          return {
-            topic: linkData[0],
-            link: linkData[1]
-          };
-        })
-      };
-    });
+    .filter(isntEmpty)
+    .map(trimEmptyEdges)
+    .map(parseBlock);
+
+
+  function isntEmpty(str) { return !!str; }
+
+  function trimEmptyEdges(str) { return str.trim(); }
+
+  function parseLink(str) {
+    const linkData = str.split(linkItemDelimiter);
+    return { topic: linkData[0], link: linkData[1] };
+  }
+
+  function parseBlock(str) {
+    const blockData = str.split(linkDelimiter);
+    return {
+      title: blockData[0].slice(2),
+      links: blockData.slice(1).map(parseLink)
+    };
+  }
 }
 
 
