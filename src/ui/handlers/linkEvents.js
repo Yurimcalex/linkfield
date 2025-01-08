08fed492933baf0c9a6c5e6dee0e238e71a40054
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-08 23:44:03 +0200

    Update link events - edited link displaying

--- src/ui/handlers/linkEvents.js
+++ src/ui/handlers/linkEvents.js
@@ -4,6 +4,7 @@ const openLinkCreatorBtn = document.querySelector('.link-creator-opener');
 const settingsWindow = document.querySelector('.settings-window');
 const addLinkForm = document.querySelector('.link-creator form');
 const content = document.querySelector('.content');
+const menu = document.querySelector('.category-menu');
 
 
 export function handleLinkEvents() {
@@ -33,9 +34,32 @@ export function handleLinkEvents() {
 
 		if (target.classList.contains('edit-btn')) {
 			const li = target.closest('li');
+			const list = li.closest('.link-list');
+			const category = list.dataset.category;
+			// it is needes because after add btn has clicked the new list content will be created (because of innerHTML)
+			// and so target li wont be there
+			const liInd = Array.from(list.children).indexOf(li);
+			
 			openLinkCreatorBtn.click();
 			putLinkDataIntoForm(li);
-			li.remove();
+			// when the category has changed it needs to scroll to that category
+			// when it hasnt changed it needs to replace the target link by new one
+			// because of there is no way to get new just created link before add btn clicking of the form
+			// the info about that link is able to catch with another handler
+			addLinkForm.add.addEventListener('click', function interceptAddBtnclick() {
+				const { category: newCategory } = getNewLinkData();
+				const newLink = list.querySelector('li:last-child');
+				const oldLink = list.children[liInd];
+				
+				if (category === newCategory) {	
+					oldLink.after(newLink);
+				} else {
+					menu.querySelector(`a[href="#${replaceSpace(newCategory)}"]`).click();
+				}
+				
+				oldLink.remove();
+				addLinkForm.add.removeEventListener('click', interceptAddBtnclick);
+			});
 		}
 	});
 }
@@ -63,4 +87,9 @@ function putLinkDataIntoForm(linkItem) {
 	addLinkForm.topic.value = linkTopic.textContent;
 	addLinkForm.type.value = linkType.textContent;
 	addLinkForm.category.value = linkList.dataset.category;
+}
+
+
+function replaceSpace(str) {
+	return str.split(' ').join('-');
 }
