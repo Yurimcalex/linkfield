f8369784099bfdad7a6c42b9d91f86e525c09a44
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-11 17:28:42 +0200

    Collect class names

---
+++ src/ui/elements.js
@@ -0,0 +1,28 @@
+const MENU_PANEL = 					 'menu-panel';
+const CATEGORY_MENU =        'category-menu';
+const CATEGORY_MENU_ITEM =   `${CATEGORY_MENU}-item`;
+const CATEGORY_MENU_HEADER = `${CATEGORY_MENU}-header`;
+const CATEGORY_MENU_LINK =   `${CATEGORY_MENU}-content`;
+const OPEN_CATEGORY_MENU_BUTTON = 'menu-opener';
+
+
+const CONTENT =        'content';
+const LINK_CATEGORY =  'link-category';
+const LINK_LIST =      'link-list';
+const LINK_LIST_ITEM = 'link-list-item';
+const LINK_TYPE =      'link-type';
+const LINK_TOPIC =     'link-topic';
+const LINK_CONTROLS =  'link-controls';
+const EDIT_BUTTON =    'edit-btn';
+const REMOVE_BUTTON =  'remove-btn';
+
+
+const SETTINGS_WINDOW = 'settings-window';
+const SET_LINK_WINDOW = 'link-creator';
+const CLOSE_BUTTON = 'close-btn';
+const LINK_FORM = 'link-form';
+
+
+const OPEN_SETTINGS_BUTTON = 'link-creator-opener';
+
+const LOADING_COVER = 'loading-cover';
