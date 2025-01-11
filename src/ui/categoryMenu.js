2c7fa30ab0d82f504aeb1f04a0f1cc7768cce7be
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-11 18:15:11 +0200

    Update category menu template

--- src/ui/categoryMenu.js
+++ src/ui/categoryMenu.js
@@ -1,6 +1,13 @@
+import { 
+	CATEGORY_MENU_ITEM,
+	CATEGORY_MENU_HEADER,
+	CATEGORY_MENU_LINK 
+} from './elements.js';
+
+import { elements } from './elements.js';
 import { replaceSpace } from './utils.js';
 
-const menu = document.querySelector('.category-menu');
+const { categoryMenu: menu } = elements;
 
 
 export function createCategoryMenuContent(data) {
@@ -11,9 +18,9 @@ export function createCategoryMenuContent(data) {
 function createContent(items) {
 	let html = '';
 	for (let item of items) {
-		html += '<li class="category-menu-item">';
-			html += '<h2 class="category-menu-header">';
-				html += `<a class="category-menu-content" href=${`#` + replaceSpace(item)}>${item}</a>`;
+		html += `<li class="${CATEGORY_MENU_ITEM}">`;
+			html += `<h2 class="${CATEGORY_MENU_HEADER}">`;
+				html += `<a class="${CATEGORY_MENU_LINK}" href=${`#` + replaceSpace(item)}>${item}</a>`;
 			html += '</h2>';
 		html += '</li>';
 	}
