a811ffbf0f2fc6e8885dbd6095137c7d893d1826
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-11 23:33:10 +0200

    Update open link form event - show appropriate button

--- src/ui/handlers/link/openLinkForm.js
+++ src/ui/handlers/link/openLinkForm.js
@@ -1,3 +1,5 @@
+import { EDIT_BUTTON } from '../../elements.js';
+
 const openBtn = document.querySelector('.link-creator-opener');
 const settingsWindow = document.querySelector('.settings-window');
 const form = document.querySelector('.link-creator form');
@@ -7,6 +9,17 @@ export function handleOpenLinkForm() {
 		openSettingsWindow();
 		resetAddLinkForm();
 		form.edit.classList.add('hide');
+		form.add.classList.remove('hide');
+	});
+
+	content.addEventListener('click', (e) => {
+		const target = e.target;
+		if (target.classList.contains(`${EDIT_BUTTON}`)) {
+			openSettingsWindow();
+			resetAddLinkForm();
+			form.edit.classList.remove('hide');
+			form.add.classList.add('hide');
+		}
 	});
 }
 
