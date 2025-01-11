23078b5be2540440f24ad54aea1cfefe8090f5d5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-11 18:57:03 +0200

    Update linkItem template

--- src/ui/linkItem.js
+++ src/ui/linkItem.js
@@ -1,19 +1,30 @@
+import {
+	LINK_LIST_ITEM,
+	LINK_TYPE,
+	LINK_TOPIC,
+	LINK_CONTROLS,
+	EDIT_BUTTON,
+	REMOVE_BUTTON
+} from './elements.js';
+import { elements } from './elements.js';
+
+
 export function createLink(data) {
-	document
-		.querySelector(`ul[data-category="${data.category}"]`)
+	elements
+		.select('linkList')(data.category)
 		.innerHTML += createLinkItem(data);
 }
 
 
 export function createLinkItem(data) {
 	let html = '';
-	html += '<li class="link-list-item">';
+	html += `<li class="${LINK_LIST_ITEM}">`;
 		html += '<h3>';
-			html += `<span class="link-type">${data.type}</span> `;
-			html += `<a class="link-topic" href=${data.link} target="_blank">${data.topic}</a>`;
-			html += '<div class="link-controls visibility sm">';
-				html += `<span><a class="link-btn edit-btn">&#128393;</a></span>`;
-				html += '<span><a class="link-btn remove-btn">&#128473;</a></span>';
+			html += `<span class="${LINK_TYPE}">${data.type}</span> `;
+			html += `<a class="${LINK_TOPIC}" href=${data.link} target="_blank">${data.topic}</a>`;
+			html += `<div class="${LINK_CONTROLS} visibility sm">`;
+				html += `<span><a class="link-btn ${EDIT_BUTTON}">&#128393;</a></span>`;
+				html += `<span><a class="link-btn ${REMOVE_BUTTON}">&#128473;</a></span>`;
 			html += '</div>';
 		html += '</h3>';
 	html += '</li>';
