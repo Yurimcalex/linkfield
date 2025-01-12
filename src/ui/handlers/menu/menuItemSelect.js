a4338ffa562fead5b46b974472cc9e8540cd15ff
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-12 15:56:08 +0200

    Update menu item select module to use elements

--- src/ui/handlers/menu/menuItemSelect.js
+++ src/ui/handlers/menu/menuItemSelect.js
@@ -1,4 +1,7 @@
-const menu = document.querySelector('.category-menu');
+import { CATEGORY_MENU_ITEM, CATEGORY_MENU_LINK, elements } from '../../elements.js';
+
+const { categoryMenu: menu } = elements;
+
 
 export const menuInfo = {
 	currentItem: null
@@ -7,7 +10,7 @@ export const menuInfo = {
 export function handleMenuItemSelect() {
 	menu.addEventListener('click', (e) => {
 		const target = e.target;
-		if (target.classList.contains('category-menu-content')) {
+		if (target.classList.contains(`${CATEGORY_MENU_LINK}`)) {
 			select(target);
 		}
 	});
@@ -15,7 +18,7 @@ export function handleMenuItemSelect() {
 
 
 function select(menuItem) {
-	const container = menuItem.closest('.category-menu-item');
+	const container = menuItem.closest(`.${CATEGORY_MENU_ITEM}`);
 	if (container === menuInfo.currentItem) return;
 	container.classList.add('highlight');
 	if (menuInfo.currentItem) menuInfo.currentItem.classList.remove('highlight');
