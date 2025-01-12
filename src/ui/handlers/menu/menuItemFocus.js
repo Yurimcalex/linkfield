901d0764ccae4ebf56323b2183aad28ddd1fbde8
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-12 10:57:23 +0200

    Update menu item focus module to use elements

--- src/ui/handlers/menu/menuItemFocus.js
+++ src/ui/handlers/menu/menuItemFocus.js
@@ -1,18 +1,18 @@
+import { LINK_CATEGORY, CATEGORY_MENU_ITEM, elements } from '../../elements.js';
 import { menuInfo } from './menuItemSelect.js';
-import { replaceSpace } from '../../utils.js';
- 
-const menu = document.querySelector('.category-menu');
-const content = document.querySelector('.content');
+
+const { categoryMenu: menu, content } = elements;
+
 
 export function handleMenuItemFocus() {
 	content.addEventListener('scroll', (e) => {
 		const centerX = document.documentElement.clientWidth / 2;
 		const elem = document.elementFromPoint(centerX, 20);
-		const container = elem.closest('.link-category');
+		const container = elem.closest(`.${LINK_CATEGORY}`);
 		if (container) {
 			const category = container.dataset.category;
-			const menuLink = menu.querySelector(`a[href="#${replaceSpace(category)}"]`);
-			const menuItem = menuLink.closest('li');
+			const menuLink = elements.select(menu, 'categoryMenuLink', category);
+			const menuItem = menuLink.closest(`.${CATEGORY_MENU_ITEM}`);
 
 			if (menuItem.classList.contains('highlight')) {
 				return;
@@ -28,8 +28,6 @@ export function handleMenuItemFocus() {
 
 
 function scrollMenuItemIntoView(menu, item) {
-	const scrollContainer = menu.closest('.menu-panel');
-	const scrollY = scrollContainer.scrollTop;
 	const itemCoords = item.getBoundingClientRect();
 	if (itemCoords.top < 0) {
 		item.scrollIntoView();
