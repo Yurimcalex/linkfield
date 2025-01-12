2b86d2bf882a4372eeae89bb40b548884cc553f9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-12 10:32:42 +0200

    Update link remove module to use elements

--- src/ui/handlers/link/linkRemove.js
+++ src/ui/handlers/link/linkRemove.js
@@ -1,12 +1,14 @@
+import { REMOVE_BUTTON, LINK_LIST_ITEM, elements } from '../../elements.js';
 import { displayControls } from './linkHover.js';
 
-const content = document.querySelector('.content');
+const { content } = elements;
+
 
 export function handleLinkRemove() {
 	content.addEventListener('click', (e) => {
 		const target = e.target;
-		if (target.classList.contains('remove-btn')) {
-			const link = target.closest('li');
+		if (target.classList.contains(`${REMOVE_BUTTON}`)) {
+			const link = target.closest(`.${LINK_LIST_ITEM}`);
 			const nextLink = link.nextElementSibling;
 			if (nextLink) displayControls(nextLink);
 			link.remove();
