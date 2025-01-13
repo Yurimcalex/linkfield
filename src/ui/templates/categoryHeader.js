be706c9b4b0b939eed54c984304e6d5e06e373cc
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-13 21:43:45 +0200

    Create select element for sorting links by type

--- src/ui/templates/categoryHeader.js
+++ src/ui/templates/categoryHeader.js
@@ -1,7 +1,21 @@
 import { replaceSpace } from '../utils.js';
 
-export function createCategoryHeader(category) {
+export function createCategoryHeader(category, linkTypes) {
 	let html = '';
-	html += `<h2 id=${replaceSpace(category)}>${category}</h2>`;
+	html += `<h2 id=${replaceSpace(category)}>`;
+		html += `<span>${category}</span>`;
+		html += `<span>Sort by ${createSelect(linkTypes)}</span>`;
+	html += '</h2>';
+	return html;
+}
+
+
+function createSelect(types) {
+	let html = '';
+	html += '<select name="sort-by-type">';
+	for (let type of types) {
+		html += `<option value=${replaceSpace(type)}>${type}</option>`;
+	}
+	html += '</select>';
 	return html;
 }
