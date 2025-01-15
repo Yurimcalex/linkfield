bc44703cf8d2e38ddcc0d38d681614c39e185b9f
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-15 21:54:29 +0200

    feat: update link counter when link is added

--- src/ui/handlers/menu/linkCounterUpdate.js
+++ src/ui/handlers/menu/linkCounterUpdate.js
@@ -1,10 +1,12 @@
 import { REMOVE_BUTTON, elements } from '../../elements.js';
+import { getFormData } from '../link/linkDisplay.js';
 
 const { content, linkForm, categoryMenu } = elements;
 
 
 export function handleLinkCounterUpdate() {
 	content.addEventListener('click', decreaseCounter);
+	linkForm.add.addEventListener('click', increaseCounter);
 }
 
 
@@ -13,8 +15,19 @@ function decreaseCounter(e) {
 	if (target.classList.contains(`${REMOVE_BUTTON}`)) {
 		const list = target.closest('[data-category]');
 		const category = list.dataset.category;
-		const linkCounter = elements.select(categoryMenu, 'categoryMenuLinkCounter', category);
-		const value = Number(linkCounter.textContent);
-		linkCounter.textContent = value - 1;
+		updateCounter(category, -1);
 	}
 }
+
+
+function increaseCounter(e) {
+	const { category } = getFormData(linkForm);
+	updateCounter(category, 1);
+}
+
+
+function updateCounter(category, newValue) {
+	const linkCounter = elements.select(categoryMenu, 'categoryMenuLinkCounter', category);
+	const currentValue = Number(linkCounter.textContent);
+	linkCounter.textContent = currentValue + newValue;
+} 
