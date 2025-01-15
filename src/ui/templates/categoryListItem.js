6ac5fe51279ca38dad0112d1f9fe4137e9439e87
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-15 16:42:37 +0200

    Separate category list item template from the data structure

--- src/ui/templates/categoryListItem.js
+++ src/ui/templates/categoryListItem.js
@@ -8,7 +8,6 @@ import {
 } from '../elements.js';
 import { elements } from '../elements.js';
 
-const { content } = elements;
 
 export function createLink(data) {
 	elements
@@ -17,12 +16,12 @@ export function createLink(data) {
 }
 
 
-export function createLinkItem(data) {
+export function createСategoryListItem(linkSrc, linkType, linkDescription) {
 	let html = '';
 	html += `<li class="${LINK_LIST_ITEM}">`;
 		html += '<h3>';
-			html += `<span class="${LINK_TYPE}">${data.type}</span> `;
-			html += `<a class="${LINK_TOPIC}" href=${data.link} target="_blank">${data.topic}</a>`;
+			html += `<span class="${LINK_TYPE}">${linkType}</span> `;
+			html += `<a class="${LINK_TOPIC}" href=${linkSrc} target="_blank">${linkDescription}</a>`;
 			html += `<div class="${LINK_CONTROLS} visibility sm">`;
 				html += `<span><a class="link-btn ${EDIT_BUTTON}">&#128393;</a></span>`;
 				html += `<span><a class="link-btn ${REMOVE_BUTTON}">&#128473;</a></span>`;
