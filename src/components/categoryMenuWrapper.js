91787bc568c0d8742c6868d79811d614889bcc18
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-16 19:03:54 +0200

    Configure category menu component

--- src/components/categoryMenuWrapper.js
+++ src/components/categoryMenuWrapper.js
@@ -1,15 +1,37 @@
 import Menu from './categoryMenu.js';
 import { selectCategoryData } from '../redux/linksSlice.js';
+import { selectMenuCategory } from '../redux/uiSlice.js';
+import { menuCategorySelected } from '../redux/uiSlice.js';
+
 
 export const categoryMenu = {
 	component: null,
+	selectCategories: null,
+	selectCategory: null,
+	dispatch: null,
+	categories: null,
+	category: null,
 
 	mount(store) {
-		const selectData = store.useSelector(selectCategoryData);
-		this.component = new Menu(selectData());
+		this.selectCategories = store.useSelector(selectCategoryData);
+		this.selectCategory = store.useSelector(selectMenuCategory);
+		this.dispatch = store.useDispatch();
+
+		const categories = this.selectCategories();
+		const category = this.selectCategory();
+		const handleClick = (category) => this.dispatch(menuCategorySelected(category));
+
+		this.component = new Menu(categories, category, handleClick);
+		this.categories = categories;
+		this.category = category;
 	},
 
-	update(store) {
-		
+	update() {
+		const category = this.selectCategory();
+
+		if (category !== this.category) {
+		 	this.component.update(category);
+		 	this.category = category;
+		}
 	}
 };
