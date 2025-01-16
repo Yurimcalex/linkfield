4580af2a8640f667cb61b403fde9feadaf8a0d49
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-16 13:29:03 +0200

    Create useSelector and useDispatch wrappers

--- src/redux/redux.js
+++ src/redux/redux.js
@@ -2,5 +2,15 @@ import { initStore } from './store.js';
 
 export function createStore(data) {
 	const store = initStore(data);
-	return store;
+	return {
+		store,
+		useSelector(fn) {
+			return (...args) => {
+				return fn(store.getState(), ...args);
+			};
+		},
+		useDispatch() {
+			return store.dispatch;
+		}
+	};
 }
