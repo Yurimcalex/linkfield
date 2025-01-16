0b9e4490d2644fd758fa9768160908effd4229ba
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-16 12:25:16 +0200

    Create empty redux store

--- src/main.js
+++ src/main.js
@@ -2,19 +2,27 @@ import parseText from './parser.js';
 import createUI from './ui/ui.js';
 
 
-async function readTextFromFile(file) {
-  const response = await fetch(file);
-  const text = await response.text();
-  return text;
-}
+
+import { configureStore } from './redux/redux.js';
+
+
 
 
 async function render() {
   const textData = await readTextFromFile('initialLinkList.txt');
   const linksData = parseText(textData);
   
-  createUI(linksData);
+  configureStore();
+  //createUI(linksData);
 }
 
 
 render();
+
+
+
+async function readTextFromFile(file) {
+  const response = await fetch(file);
+  const text = await response.text();
+  return text;
+}
