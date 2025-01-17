f6dc7b2f93219ffbd582d82f346a9d7d26554df6
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 22:15:50 +0200

    Scroll to selected category feat

--- src/components/common/Component.js
+++ src/components/common/Component.js
@@ -1,5 +1,12 @@
+import { CONTENT } from '../classNames.js';
+import { replaceSpace } from '../../ui/utils.js';
+
+
 export default class Component {
 	constructor(storeAction) {
+		this.content = document.querySelector(`.${CONTENT}`);
+		this.DY = 50; // takes into account the height of the menu bar
+
 		window.addEventListener('resize', () => {
 			storeAction(this.getIsSmallScreen());
 		});
@@ -8,5 +15,19 @@ export default class Component {
 	getIsSmallScreen() {
 	  const SMALL_SCREEN_WIDTH = 650;
 	  return window.innerWidth <= SMALL_SCREEN_WIDTH;
+	}
+
+	// scroll the window for large screen, scroll the content for small screen
+	scrollContentTo(category, isSmallScreen) {
+		if (!category) return;
+		const id = replaceSpace(category);
+		const categoryHeader = document.getElementById(id);
+		const container = isSmallScreen ? window : this.content;
+		container.scrollTo(0, categoryHeader.offsetTop - (isSmallScreen ? this.DY : 0));
+		history.pushState(null, null, id);
+	}
+
+	update(category, isSmallScreen) {
+		this.scrollContentTo(category, isSmallScreen);
 	}
 }
