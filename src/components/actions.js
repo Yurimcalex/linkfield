b30badf645daeada700fc4f853f7413c76ca7dbf
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 13:35:06 +0200

    Create click category action when the screen size is small

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,7 +1,13 @@
-import { menuCategorySelected, categoryMenuToggled } from '../redux/uiSlice.js';
+import { menuCategorySelected, categoryMenuToggled, screenSizeChanged } from '../redux/uiSlice.js';
 
 export const clickCategoryMenu = (dispatch) =>
 	(category) => dispatch(menuCategorySelected(category));
 
 export const clickMenuOpener = (dispatch) =>
+	() => dispatch(categoryMenuToggled());
+
+export const changeScreenSize = (dispatch) =>
+	(isSmall) => dispatch(screenSizeChanged(isSmall));
+
+export const clickCategoryMenuOnSmallScreen = (dispatch) =>
 	() => dispatch(categoryMenuToggled());
