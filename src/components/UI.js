a7bc6bd47064a98c7a730e57b3a6adaaa4f8aef5
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 10:54:11 +0200

    Initiate Opener component

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,16 +1,20 @@
 import CategoryMenu from './categoryMenu/MenuWrapper.js';
+import MenuOpener from './categoryMenuOpener/OpenerWrapper.js';
 
 
 export default class UI {
 	constructor(store) {
 		this.categoryMenu = new CategoryMenu(store);
+		this.menuOpener = new MenuOpener(store);
 	}
 
 	mount() {
 		this.categoryMenu.mount();
+		this.menuOpener.mount();
 	}
 
 	update() {
 		this.categoryMenu.update();
+		this.menuOpener.update();
 	}
 }
