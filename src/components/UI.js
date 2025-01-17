695eae78bed3fb803d80417fe91d0054154aab56
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 13:41:52 +0200

    Initiate common Component

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,20 +1,23 @@
 import CategoryMenu from './categoryMenu/MenuWrapper.js';
 import MenuOpener from './categoryMenuOpener/OpenerWrapper.js';
-
+import CommonComponent from './common/ComponentWrapper.js';
 
 export default class UI {
 	constructor(store) {
 		this.categoryMenu = new CategoryMenu(store);
 		this.menuOpener = new MenuOpener(store);
+		this.commonComponent = new CommonComponent(store);
 	}
 
 	mount() {
 		this.categoryMenu.mount();
 		this.menuOpener.mount();
+		this.commonComponent.mount();
 	}
 
 	update() {
 		this.categoryMenu.update();
 		this.menuOpener.update();
+		this.commonComponent.update();
 	}
 }
