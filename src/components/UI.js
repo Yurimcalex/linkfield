c442e9e228d2d953f4c4b726e88a425d91979a8f
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 17:26:53 +0200

    Initiate newly created components

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,23 +1,28 @@
 import CategoryMenu from './categoryMenu/MenuWrapper.js';
 import MenuOpener from './categoryMenuOpener/OpenerWrapper.js';
 import CommonComponent from './common/ComponentWrapper.js';
+import Content from './linkCategories/ContentWrapper.js';
+
 
 export default class UI {
 	constructor(store) {
 		this.categoryMenu = new CategoryMenu(store);
 		this.menuOpener = new MenuOpener(store);
 		this.commonComponent = new CommonComponent(store);
+		this.content = new Content(store);
 	}
 
 	mount() {
 		this.categoryMenu.mount();
 		this.menuOpener.mount();
 		this.commonComponent.mount();
+		this.content.mount();
 	}
 
 	update() {
 		this.categoryMenu.update();
 		this.menuOpener.update();
 		this.commonComponent.update();
+		this.content.update();
 	}
 }
