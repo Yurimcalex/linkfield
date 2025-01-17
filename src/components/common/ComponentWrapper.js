bc73039080f4fcc6e268f1a8dd9e67e39df8beac
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-17 22:14:38 +0200

    Pass is small screen flag through Component wrapper

--- src/components/common/ComponentWrapper.js
+++ src/components/common/ComponentWrapper.js
@@ -1,15 +1,22 @@
 import Component from './Component.js';
+import { selectMenuCategory, selectIsSmallScreen } from '../../redux/uiSlice.js';
 import { changeScreenSize } from '../actions.js';
 
 
 export default class Wrapper {
 	constructor(store) {
 		this.storeAction = changeScreenSize(store.useDispatch());
+		this.selectMenuCategory = store.useSelector(selectMenuCategory);
+		this.selectIsSmallScreen = store.useSelector(selectIsSmallScreen);
 	}
 
 	mount() {
 		this.component = new Component(this.storeAction);
 	}
 
-	update() {}
+	update() {
+		const selectedCategory = this.selectMenuCategory();
+		const isSmallScreen = this.selectIsSmallScreen();
+		this.component.update(selectedCategory, isSmallScreen);
+	}
 }
