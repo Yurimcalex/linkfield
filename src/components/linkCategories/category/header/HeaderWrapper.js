3c60b03b976116116b64de6607d03f4fe6e8dc07
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-18 17:49:30 +0200

    Provide store action to Header component

--- src/components/linkCategories/category/header/HeaderWrapper.js
+++ src/components/linkCategories/category/header/HeaderWrapper.js
@@ -1,17 +1,23 @@
 import Header from './Header.js';
 import { selectLinkTypesByCategory } from '../../../../redux/linksSlice.js';
+import { selectLinkType } from '../../../../redux/filtersSlice.js';
+import { selectCategoryLinkType } from '../../../actions.js';
 
 
 export default class HeaderWrapper {
 	constructor(store, category) {
 		this.selectLinkTypes = store.useSelector(selectLinkTypesByCategory);
+		this.selectLinkType = store.useSelector(selectLinkType);
+		this.setLinkType = selectCategoryLinkType(store.useDispatch());
 		this.category = category;
 	}
 
 	mount() {
 		const linkTypes = this.selectLinkTypes(this.category);
-		this.component = new Header(this.category, linkTypes);
+		const linkType = this.selectLinkType(this.category);
+		this.component = new Header(this.category, linkTypes, this.setLinkType);
 		this.linkTypes = linkTypes;
+		this.linkType = linkType;
 	}
 
 	update() {
