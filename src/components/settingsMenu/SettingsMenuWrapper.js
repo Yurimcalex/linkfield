0dd18954c9cce5e4011b4f72f1f4b8504e76728d
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-20 12:55:27 +0200

    Set link form mode to 'creation' when clicking on add new link btn

--- src/components/settingsMenu/SettingsMenuWrapper.js
+++ src/components/settingsMenu/SettingsMenuWrapper.js
@@ -1,14 +1,19 @@
 import SettingsMenu from './SettingsMenu.js';
-import { toggleSettingWindow } from '../actions.js';
+import { toggleSettingWindow, changeLinkFormMode } from '../actions.js';
 
 
 export default class SettingsMenuWrapper {
 	constructor(store) {
-		this.toggleSettingWindow = toggleSettingWindow(store.useDispatch())
+		this.toggleSettingWindow = toggleSettingWindow(store.useDispatch());
+		this.changeLinkFormMode = changeLinkFormMode(store.useDispatch());
+		this.openLinkFormForCreation = () => {
+			this.toggleSettingWindow();
+			this.changeLinkFormMode('creation');
+		};
 	}
 
 	mount() {
-		this.component = new SettingsMenu(this.toggleSettingWindow);
+		this.component = new SettingsMenu(this.openLinkFormForCreation);
 	}
 
 	update() {}
