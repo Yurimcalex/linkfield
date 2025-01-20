bd430db4d04429d09b8a922ffe4586945cea5a99
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-20 13:52:46 +0200

    Provide stora date to the Link form to enable a new link creation

--- src/components/settingsWindow/linkForm/LinkFormWrapper.js
+++ src/components/settingsWindow/linkForm/LinkFormWrapper.js
@@ -1,6 +1,7 @@
 import LinkFrom from './LinkForm.js';
 import { selectCategoryNames, selectLinkTypes } from '../../../redux/linksSlice.js';
 import { selectLinkFormMode } from '../../../redux/uiSlice.js';
+import { toggleSettingWindow, changeLinkFormMode, createLink } from '../../actions.js';
 
 
 export default class LinkFromWrapper {
@@ -8,13 +9,21 @@ export default class LinkFromWrapper {
 		this.selectCategoryNames = store.useSelector(selectCategoryNames);
 		this.selectLinkTypes = store.useSelector(selectLinkTypes);
 		this.selectLinkFormMode = store.useSelector(selectLinkFormMode);
+		this.toggleSettingWindow = toggleSettingWindow(store.useDispatch());
+		this.changeLinkFormMode = changeLinkFormMode(store.useDispatch());
+		this.createLink = createLink(store.useDispatch());
+		this.createLinkAction = (linkData) => {
+			this.toggleSettingWindow();
+			this.changeLinkFormMode('');
+			this.createLink(linkData);
+		};
 	}
 
 	mount() {
 		const categories = this.selectCategoryNames();
 		const types = this.selectLinkTypes();
 		const mode = this.selectLinkFormMode();
-		this.component = new LinkFrom(categories, types);
+		this.component = new LinkFrom(categories, types, this.createLinkAction);
 		this.categories = categories;
 		this.types = types;
 		this.mode = mode;
