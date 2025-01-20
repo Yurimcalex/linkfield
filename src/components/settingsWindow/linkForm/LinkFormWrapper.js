6a7cc5de0a117e275fd82aff358f15251e1bbf1c
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-20 13:00:42 +0200

    Initiate Link form update when the form mode is changed

--- src/components/settingsWindow/linkForm/LinkFormWrapper.js
+++ src/components/settingsWindow/linkForm/LinkFormWrapper.js
@@ -1,22 +1,30 @@
 import LinkFrom from './LinkForm.js';
 import { selectCategoryNames, selectLinkTypes } from '../../../redux/linksSlice.js';
+import { selectLinkFormMode } from '../../../redux/uiSlice.js';
 
 
 export default class LinkFromWrapper {
 	constructor(store) {
 		this.selectCategoryNames = store.useSelector(selectCategoryNames);
 		this.selectLinkTypes = store.useSelector(selectLinkTypes);
+		this.selectLinkFormMode = store.useSelector(selectLinkFormMode);
 	}
 
 	mount() {
 		const categories = this.selectCategoryNames();
 		const types = this.selectLinkTypes();
+		const mode = this.selectLinkFormMode();
 		this.component = new LinkFrom(categories, types);
 		this.categories = categories;
 		this.types = types;
+		this.mode = mode;
 	}
 
 	update() {
-		this.component.update();
+		const mode = this.selectLinkFormMode();
+		if (mode !== this.mode) {
+			this.component.update(mode);
+			this.mode = mode;
+		}
 	}
 }
