f9ff65d62b4ba1a6f599d21450504ddeed52d5f9
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-23 14:24:14 +0200

    Update the category menu opener only when appropriate actions are happened

--- src/components/categoryMenuOpener/OpenerWrapper.js
+++ src/components/categoryMenuOpener/OpenerWrapper.js
@@ -1,27 +1,29 @@
 import Opener from './Opener.js';
 import { useSelector, useDispatch } from '../../redux/redux.js';
 import { selectMenuVisibility } from '../../redux/uiSlice.js';
+import { selectAction } from '../../redux/actionSlice.js';
 import { toggleMenuOpener } from '../actions.js';
 
 
 export default class Wrapper {
 	constructor(store) {
 		this.component = null;
-		this.isMenuOpened = null;
-		useSelector(this, store, [ selectMenuVisibility ]);
+		useSelector(this, store, [ selectMenuVisibility, selectAction ]);
 		useDispatch(this, store, [ toggleMenuOpener ]);
+
+		this.updateActions = {
+			'ui/categoryMenuToggled': true
+		};
 	}
 
 	mount() {
 		this.component = new Opener(this.toggleMenuOpener);
-		this.isMenuOpened = this.selectMenuVisibility();
 	}
 
 	update() {
-		const isMenuOpened = this.selectMenuVisibility();
-		if (isMenuOpened !== this.isMenuOpened) {
-			this.component.update(isMenuOpened);
-			this.isMenuOpened = isMenuOpened;
-		}
+		const action = this.selectAction();
+		if (!(action in this.updateActions)) return;
+
+		this.component.update(this.selectMenuVisibility());
 	}
 }
