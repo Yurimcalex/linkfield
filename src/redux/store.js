0b045bb91d8848e68fb99411c8d6ba85cd6682cd
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-23 12:58:06 +0200

    Create action store enhancer to dispatch an addition action

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import linksReducer from './linksSlice.js';
 import uiReducer from './uiSlice.js';
 import filtersSlice from './filtersSlice.js';
+import actionSlice, { actionHappened } from './actionSlice.js';
 
 
 export function initStore(data) {
@@ -14,9 +15,23 @@ export function initStore(data) {
 		},
 
 		middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logActionType),
-		
+		enhancers: (getDefaultEnhancers) => getDefaultEnhancers().concat(actionEnhancer),
 		preloadedState: data
 	});
+}
+
+
+const actionEnhancer = (createStore) => {
+  return (reducer, state, enhancer) => {
+    const store = createStore(reducer, state, enhancer)
+    const newDispatch = (action) => {
+      const result = store.dispatch(action);
+      store.dispatch(actionHappened(action.type));
+      return result;
+    }
+
+    return { ...store, dispatch: newDispatch };
+  }
 }
 
 
