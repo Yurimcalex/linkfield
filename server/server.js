c6f53acf43ced4c5c9d4b956d805f9ba85ae4560
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-24 16:24:00 +0200

    Create insert links fn

--- server/server.js
+++ server/server.js
@@ -9,15 +9,50 @@ async function run() {
 		await client.connect();
 		const db = client.db('Links');
 		const coll = db.collection('links');
-		const cursor = coll.find({});
 
-		await cursor.forEach(console.log);
+		//const allValues = await getAllLinks(coll);
+		//console.log(allValues);
+		
+		const insertedDocs = await insertLinks(coll, [
+			{
+				description: 'React documentation',
+				src: 'https://react.dev/learn',
+				category: 'react',
+				type: 'docs'
+			},
+
+			{
+				description: 'JavaScript Game Tutorial – Build a Stick Hero Clone with HTML Canvas + JavaScript',
+				src: 'https://www.freecodecamp.org/news/javascript-game-tutorial-stick-hero-with-html-canvas/',
+				category: 'javascript',
+				type: 'tutorial'
+			},
+
+			{
+				description: 'JavaScript Game Dev Tutorial – Build Gorillas with HTML Canvas + JavaScript',
+				src: 'https://www.freecodecamp.org/news/gorillas-game-in-javascript/',
+				category: 'javascript',
+				type: 'tutorial'
+			}
+		]);
+
+		console.log(insertedDocs);
 
 	} finally {
 		await client.close();
 	}
 }
 
-run().catch(err => console.log(err));
 
-console.log(uri);
+async function getAllLinks(links_coll) {
+	return await links_coll.find().toArray();
+}
+
+
+async function insertLinks(links_coll, docs) {
+	return await links_coll.insertMany(docs);
+}
+
+
+
+run().catch(err => console.log(err));
