22c1c5fa093af77ba141e052d522c68f333774d4
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-24 15:32:31 +0200

    Establish connection to mongodb

--- server/server.js
+++ server/server.js
@@ -1,3 +1,23 @@
 import 'dotenv/config';
+import { MongoClient } from 'mongodb';
 
-console.log(process.env.ATLAS_URI);
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
+
+async function run() {
+	try {
+		await client.connect();
+		const db = client.db('Links');
+		const coll = db.collection('links');
+		const cursor = coll.find({});
+
+		await cursor.forEach(console.log);
+
+	} finally {
+		await client.close();
+	}
+}
+
+run().catch(err => console.log(err));
+
+console.log(uri);
