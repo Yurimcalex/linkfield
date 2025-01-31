ee62fa315ef7dadd28226081bc60ea5913e854f7
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-01 00:01:31 +0200

    Create create link api meth

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,18 @@ const api = {
 	loadLinks: async () => {
 		let result = await fetch(`${baseUrl}/links`);
 		return await result.json();
+	},
+
+	createLink: async (linkData) => {
+		const response = await fetch(`${baseUrl}/links`, {
+			method: 'POST',
+			headers: {
+			  "content-type": "application/json"
+			},
+			body: JSON.stringify(linkData)
+		});
+		const result = await response.json();
+		return { ...linkData, _id: result.insertedId };
 	}
 };
 
@@ -11,7 +23,13 @@ const api = {
 test();
 
 async function test() {
-	const result = await api.loadLinks();
+	const result = await api.createLink({
+		description: 'Some description',
+		category: 'Some category',
+		src: 'Some src',
+		type: 'Some type'
+	});
+
 	console.log(result);
 }
 
