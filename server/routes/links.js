289fd51074aea9639d32c309b397e67c0dfff503
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-01-31 19:46:20 +0200

    Create get all links route

--- server/routes/links.js
+++ server/routes/links.js
@@ -6,7 +6,11 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-	res.send('OK!!!');
+	const collection = await db.collection('links');
+	const result = await collection
+		.find({})
+		.toArray();
+	res.send(result).status(200);
 });
 
 export default router;
