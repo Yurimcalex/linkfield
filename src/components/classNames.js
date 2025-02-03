4379a71e6b47928c8cac8b9caad583cd8f454fb1
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-03 21:49:07 +0200

    style: create link window block and relative elements

--- src/components/classNames.js
+++ src/components/classNames.js
@@ -10,35 +10,37 @@ export const CATEGORY_MENU_OPENER_HIDE = 'page__menu-opener_hidden';
 
 
 
-export const CONTENT =              'content';
-export const CONTENT_HIDE =         'page__content_hidden';
-export const LINK_CATEGORY =        'category';
-export const LINK_CATEGORY_HEADER = 'category__header';
-export const LINK_CATEGORY_HEADER_HIDE = 'category__header_hidden';
+export const CONTENT =                       'content';
+export const CONTENT_HIDE =                  'page__content_hidden';
+export const LINK_CATEGORY =                 'category';
+export const LINK_CATEGORY_HEADER =          'category__header';
+export const LINK_CATEGORY_HEADER_HIDE =     'category__header_hidden';
 export const LINK_CATEGORY_HEADER_SORTNAME = 'category__header-sortname';
-export const LINK_CATEGORY_HEADER_SELECT = 'category__header-select';
+export const LINK_CATEGORY_HEADER_SELECT =   'category__header-select';
 
-export const LINK_LIST =            'link-list';
-export const LINK_LIST_ITEM =       'link-list__item';
+
+export const LINK_LIST =              'link-list';
+export const LINK_LIST_ITEM =         'link-list__item';
 export const LINK_LIST_ITEM_CONTENT = 'link-list__item-content';
-export const LINK_TYPE =            'link-list__item-type';
-export const LINK_TOPIC =           'link-list__item-description';
-export const LINK_CONTROLS =        'link-list__item-controls';
-export const LINK_CONTROLS_HIDE =   'link-list__item-controls_hidden';
-export const LINK_CONTROLS_SM_SC =  'page__link-list-controls_adaptive';
-export const LINK_BUTTON =          'link-list__item-button';
-export const EDIT_BUTTON =          'link-list__item-button_edit';
-export const REMOVE_BUTTON =        'link-list__item-button_close';
+export const LINK_TYPE =              'link-list__item-type';
+export const LINK_TOPIC =             'link-list__item-description';
+export const LINK_CONTROLS =          'link-list__item-controls';
+export const LINK_CONTROLS_HIDE =     'link-list__item-controls_hidden';
+export const LINK_CONTROLS_SM_SC =    'page__link-list-controls_adaptive';
+export const LINK_BUTTON =            'link-list__item-button';
+export const EDIT_BUTTON =            'link-list__item-button_edit';
+export const REMOVE_BUTTON =          'link-list__item-button_close';
 
 
-export const SETTINGS_MENU =   'settings-menu';
-export const SETTINGS_WINDOW = 'settings-window';
-export const SETTINGS_WINDOW_HIDE = 'page__settings-window_hidden';
-       const SET_LINK_WINDOW = 'link-creator';
-export const CLOSE_BUTTON =    'close-btn';
-export const LINK_FORM =       'link-form';
-
-
+export const SETTINGS_MENU =        'settings-menu';
 export const OPEN_SETTINGS_BUTTON = 'settings-menu__link-form-opener';
+
+
+export const SETTINGS_WINDOW =      'settings-window';
+export const SETTINGS_WINDOW_HIDE = 'page__settings-window_hidden';
+       const SET_LINK_WINDOW =      'link-window';
+export const CLOSE_BUTTON =         'link-window__close-btn';
+export const LINK_FORM =            'link-window__form';
+
 
 const LOADING_COVER = 'loading-cover';
