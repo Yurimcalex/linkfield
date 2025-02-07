d2313a2cfaa69e375c1c36ae00d24c994f09eca4
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-07 20:23:14 +0200

    Update subscribed fn to store changes

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { getCategoryFromHash, getIsSmallScreen } from './components/utils.js';
 import api from './api/api.js';
 import fakeApi from './fakeApi/api.js';
 import { initiateDB } from './fakeApi/db.js';
+import { linksLoaded } from './redux/linksSlice.js';
+
+
+import { userLogged } from './redux/userSlice.js';
+
 
 async function render() {
   await initiateDB();
@@ -19,9 +24,31 @@ async function render() {
   };
 
   const store = createStore(initialData);
-  const ui = new UI(store);
+  let ui = new UI(store);
   ui.mount();
-  store.store.subscribe(() => ui.update());
+
+  let isLogged = false;
+  store.store.subscribe(() => {
+    const state = store.store.getState();
+    if (state.user.token && !isLogged) {
+      //store.state.dispatch(loadLinks());
+      isLogged = true;
+      console.log('User logged!');
+    }
+
+    if (state.action.action === 'links/linksLoaded') {
+      //let ui = new UI(store);
+      //ui.mount();
+      console.log('Links from db loaded!');
+    }
+
+    ui.update();
+  });
+
+
+  // setTimeout(() => {
+  //   store.store.dispatch(userLogged(email, password));
+  // }, 5000);
 }
 
 render();
