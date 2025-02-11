620a2f4b75eff48c59c9fa84c81735d0af45773a
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-11 14:52:20 +0200

    Create change theme feat

--- src/components/themeMenu/ThemeMenu.js
+++ src/components/themeMenu/ThemeMenu.js
@@ -1,10 +1,35 @@
 import { dom } from '../elements.js';
-import { THEME_MENU_HIDE } from '../classNames.js';
+import { THEME_MENU_HIDE, theme } from '../classNames.js';
 
 
 export default class ThemeMenu {
 	constructor() {
 		this.node = dom.themeMenu.get();
+		this.node.addEventListener('click', (e) => {
+			const target = e.target.closest('[data-theme]');
+			if (target) {
+				const theme = target.dataset.theme;
+				dom.themeMenu.get().classList.toggle(`${THEME_MENU_HIDE}`);
+				this.setTheme(theme);
+			}
+		});
+	}
+
+	setTheme(newTheme) {
+		const elements = document.querySelectorAll(`[class*="theme_"]`);
+		for (let element of elements) {
+			const classes = Array.from(element.classList)
+				.filter(c => c.startsWith('theme_'));
+
+			classes.forEach(oldClass => {
+				const newClass = oldClass.replace(/[a-zA-Z0-9]+$/, newTheme);
+				element.classList.replace(oldClass, newClass);
+			});
+		}
+
+		for (let prop in theme) {
+			theme[prop] = theme[prop].replace(/[a-zA-Z0-9]+$/, newTheme);
+		}
 	}
 
 	create() {}
