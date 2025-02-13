34bd7eaeb2f1d74f01eb8694a3bd3ef3e46a9419
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-13 16:20:36 +0200

    Update main module to load links after ui is rendered

--- src/main.js
+++ src/main.js
@@ -1,54 +1,30 @@
 import { createStore } from './redux/redux.js';
 import UI from './components/UI.js';
 import { getCategoryFromHash, getIsSmallScreen } from './components/utils.js';
-import api from './api/api.js';
-import fakeApi from './fakeApi/api.js';
 import { initiateDB } from './fakeApi/db.js';
 import { linksLoaded } from './redux/linksSlice.js';
+import { menuCategorySelected } from './redux/uiSlice.js';
 
 
-import { userLogged } from './redux/userSlice.js';
+const initialData = {
+	links: { data: [] },
+	ui: { 
+    menuCategory: '',
+    isMenuOpened: false,
+    isSmallScreen: getIsSmallScreen(),
+  }
+};
+
+const store = createStore(initialData);
+let ui = new UI(store);
+ui.mount();
+store.store.subscribe(() => ui.update());
 
 
 async function render() {
-  await initiateDB();
-  const links = await fakeApi.loadLinks() || [];
-
-  const initialData = {
-  	links: { data: links },
-  	ui: { 
-      menuCategory: links.length ? getCategoryFromHash(location) : '',
-      isMenuOpened: false,
-      isSmallScreen: getIsSmallScreen(),
-    }
-  };
-
-  const store = createStore(initialData);
-  let ui = new UI(store);
-  ui.mount();
-
-  let isLogged = false;
-  store.store.subscribe(() => {
-    const state = store.store.getState();
-    if (state.user.token && !isLogged) {
-      //store.state.dispatch(loadLinks());
-      isLogged = true;
-      console.log('User logged!');
-    }
-
-    if (state.action.action === 'links/linksLoaded') {
-      //let ui = new UI(store);
-      //ui.mount();
-      console.log('Links from db loaded!');
-    }
-
-    ui.update();
-  });
-
-
-  // setTimeout(() => {
-  //   store.store.dispatch(userLogged(email, password));
-  // }, 5000);
+	await initiateDB();
+	await store.store.dispatch(linksLoaded());
+	store.store.dispatch(menuCategorySelected( getCategoryFromHash() ));
 }
 
 render();
